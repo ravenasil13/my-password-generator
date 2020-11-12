579ef2be4f4d232e89fb00bdeb1e8890626c2eaa
Raven Guerrero <ravenasil13@gmail.com>
2020-11-11 23:49:33 -0600

    added conditional statements

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
 var smallChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "t", "u", "v", "w", "x", "y", "z"];
 var bigChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symbols = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
-var userChoice = []
+var pwArray = "";
 
 
-//do I need this?
-var generateBtn = document.querySelector("#generate");
 
-var confirmPassword = confirm("Would you like to create a password?");
+var passwordLength = prompt("How many characters would you like your password to contain?");
+// if password doesn't fit critera 
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Your password is too short. Please try again.");
+    // loop to present user options 
 
-//if user would like to create pw (confirmPw === true, run:)
-if (confirmPassword) {
-    alert("Click OK to create password");
+  }
+
+var charChosen = confirm("Would you like to use lowercase letters?")
+if (smallChar === true) {
+  password = []
 }
 
-//function to create password
-function createPassword
-
-
-
-
-
-//ask user how long they want their pw to be
-var pwLength = prompt("How many characters would you like your password to be?");
-//if user decides pw to be less than min. 8 characters alert
-if (pwLength <= 8) {
-    alert("Password not long enough. Please try again.")
+var charChosen = confirm("Would you like to use uppercase letters?")
+if (bigChar === true) {
+  password =[]
 }
-//else if user elects to have 8+ characters alert
-else if (pwLength => 8) {
-    alert
+
+var charChosen = confirm("Would you like to use numbers?")
+if (numbers === true) {
+  password = []
 }
+
+var charChosen = confirm("Would you like to use symbols?")
+if (symbols === true) {
+  password = []
+}
+
+
+
+
